f64f949dea37f1a801ac2b7deb4d4329874cbe89
ShannonLin24 <shannonlin2012@gmail.com>
2016-07-13 16:04:16 -0700

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,42 @@
+ var firstName = 'Shannon ';
+    var fullName = lastName + ' Lin';
+    console.log(fullName); //Outputs 'Shannon Lin'
+       
+        var numberOfKittens = 5;
+    var numberOfPuppies = 4;
+    var numberOfAnimals = numberOfKittens + numberOfPuppies;
+          function callKitten (kittenName)
+    {
+        console.log('Come here, ' + kittenName + '!');
+    }
+    callKitten ('Fluffy'); //outputs 'Come here, Fluffy!'
+    
+    function addNumbers(a, b) 
+    {
+        console.log(a + b);
+    }
+    addNumbers(5,7); //outputs 12
+    addNumbers(9,12); //outputs 21
+        
+var numberOfKittens = 10
+var numberOfDogs = 30
+           
+addNumbers(numberOfDogs, numberOfKittens)
+    console.log("Starting with example");
+    function addNumbers(a,b)
+    {
+        console.log(a + b);
+    }
+        
+    console.log("Done with example");
+       
+         alert('Come to the dark side. We have COOKIES!');
+        var numberOfRainbows = 5;
+        var numberOfSparkles = 4;
+        var numberOfUnicorns = numberOfRainbows + numberOfSparkles;
+        console.log(numberOfUnicorns)
+        
+         var kittensName = 'Fluffy ';
+    var fullName = kittensName + ' McDougle';
+    console.log(fullName); //Outputs 'Fluffy McDougle'
+       
